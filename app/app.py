edc3d92906a48727fb628757c0d223cf856c1b92
David Grisales <davidgrisales@Davids-MacBook-Air-3.local>
2026-04-19 18:29:36 -0500

    black applied

--- app/app.py
+++ app/app.py
@@ -37,5 +37,5 @@ def index():
     return render_template("index.html", resultado=resultado)
 
 
-if __name__ == "__main__":  # pragma: no cover
+if __name__ == "__main__":  # pragma: no
     app.run(debug=True, port=5000, host="127.0.0.1")
